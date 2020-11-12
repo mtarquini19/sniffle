72540e2fc71fc7bd1251e57069e95249728c58f2
MichaelT <mtarquini19@gmail.com>
2020-11-12 15:16:39 -0800

    re-did html, started js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    // assign variables for date & city search storage
+    var todaysDate = moment().format("MMM Do YYYY");
+    var cities = $(".list-group");
+    var cityButtons = [];
+
+    // display current weather
+    // var displayWeather = (cityName) function() {
+
+    });
